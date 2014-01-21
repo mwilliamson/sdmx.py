91513616384bec4e39e6429d28e60a7d71091d3e
Michael Williamson <mike@zwobble.org>
2014-01-21 14:37:30 +0000

    Bump version to 0.2.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if sys.version_info[:2] < (2, 7):
 
 setup(
     name='sdmx',
-    version='0.2.4',
+    version='0.2.5',
     description='Read SDMX XML files',
     long_description=read("README"),
     author='Michael Williamson',
